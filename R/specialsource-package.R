82c444ffddbc1d4fb214e355c271dbbb08479d8b
Jacob Scott <jacob.scott@education.gov.uk>
2023-05-16 20:09:09 +0100

    initial commit

---
+++ R/specialsource-package.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+
+#' @import roxygen2
+
+#' @importFrom roxygen2 roxy_tag_parse
+#' @keywords internal
+#' @export
+NULL
+
+#' @importFrom roxygen2 roxy_tag_rd
+#' @keywords internal
+#' @export
+NULL
+
+## usethis namespace: end
+NULL
